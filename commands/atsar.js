c5f5ad8be0a25b4f75c521720d6385ba5da506ef
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2023-03-19 17:27:27 +0100

    role update for a profile

--- commands/atsar.js
+++ commands/atsar.js
@@ -17,11 +17,11 @@ module.exports = {
         const regex = /(?<=\/)[a-zA-Z]+-\d+(?=-[a-zA-Z0-9]*\.png)/;
         // Extract the image URLs of the ranks
         const tankUrl = $('img.Profile-playerSummary--rank')[0].attribs.src;
-        const dpsUrl = $('img.Profile-playerSummary--rank')[1].attribs.src;
+        
         const suppUrl = $('img.Profile-playerSummary--rank')[2].attribs.src;
         //Use Regex on urls
         const tankRank = tankUrl.match(regex)[0];
-        const dpsRank = dpsUrl.match(regex)[0];
+        
         const suppRank = suppUrl.match(regex)[0];
             
         // reply tier rank in an embed message
@@ -30,7 +30,7 @@ module.exports = {
         .setURL('https://overwatch.blizzard.com/fr-fr/career/Atsar-2949/')
         .setFields(
             { name:'Tank', value: `${tankRank}`, inline:true},
-            {name:"DPS", value:`${dpsRank}`, inline:true},
+            
             {name:"Support", value:`${suppRank}`, inline:true}
             )
         .setTimestamp()
