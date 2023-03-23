0a8288d284889ffd642005e90c2fb227ddfea6d0
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2023-03-23 13:22:48 +0100

    update ranks for atsar's profile

--- commands/atsar.js
+++ commands/atsar.js
@@ -17,20 +17,17 @@ module.exports = {
         const regex = /(?<=\/)[a-zA-Z]+-\d+(?=-[a-zA-Z0-9]*\.png)/;
         // Extract the image URLs of the ranks
         const tankUrl = $('img.Profile-playerSummary--rank')[0].attribs.src;
-        
-        const suppUrl = $('img.Profile-playerSummary--rank')[2].attribs.src;
+        const suppUrl = $('img.Profile-playerSummary--rank')[1].attribs.src;
         //Use Regex on urls
         const tankRank = tankUrl.match(regex)[0];
-        
         const suppRank = suppUrl.match(regex)[0];
             
         // reply tier rank in an embed message
         const embed = new EmbedBuilder()
-        .setTitle('DoomFrite est actuellement classé :')
+        .setTitle('Atsar est actuellement classé :')
         .setURL('https://overwatch.blizzard.com/fr-fr/career/Atsar-2949/')
         .setFields(
             { name:'Tank', value: `${tankRank}`, inline:true},
-            
             {name:"Support", value:`${suppRank}`, inline:true}
             )
         .setTimestamp()
